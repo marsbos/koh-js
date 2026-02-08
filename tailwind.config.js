47e546dbd6d15935969bc0fd1838a2d2d1c982f9
Marcel <mbos@emico.nl>
2026-02-08 21:33:51 +0100

    Add built tailwind to docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./examples/**/*.{html,js}'],
+    content: ['./docs/**/*.{html,js}'],
     theme: {
         extend: {},
     },
